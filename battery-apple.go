d5edfc331a3c37ae5ca76e80c17759167712d7b6
Drew Nelson <drewnelson112@gmail.com>
2017-04-30 21:15:21 -0400

    Add flags for file, seperator, corpus file, password length, and randomization seed.

--- battery-apple.go
+++ battery-apple.go
@@ -3,14 +3,20 @@ package main
 import (
     "io/ioutil"
     "fmt"
+    "flag"
     "math/rand"
     "time"
     "strings"
+    // "strconv"
+    "hash/fnv"
 )
 
-func main() {
-    var corpusFile string = "corpus.txt"
-        
+
+// Retrieves a list of words from a newline seperated file
+// Returns an array of single words
+// TODO: Parameterize word source
+func loadWordList(corpusFile string) []string {
+    // var corpusFile string = "corpus.txt"
 
     // read the whole file at once
     corpus, err := ioutil.ReadFile(corpusFile)
@@ -20,10 +26,53 @@ func main() {
 
     words := strings.Split(string(corpus), "\n")
 
+    return words
+}
 
-    s := rand.NewSource(time.Now().Unix())
-    r := rand.New(s) // initialize local pseudorandom generator 
-    q := words[r.Intn(len(words))]
+func randomWord(wordList []string, r *rand.Rand) string {
 
-    fmt.Printf("%s\n", q)
+  q := wordList[r.Intn(len(wordList))]
+
+  return q
+}
+
+// Uses magic to create a hash of a string
+func hash(s string) int64 {
+        h := fnv.New64a()
+        h.Write([]byte(s))
+        return int64(h.Sum64())
+}
+
+func main() {
+
+  separatorPtr := flag.String("separator", "-", "character(s) to place between words")
+  pwdLengthPtr := flag.Int("pwdLength", 4, "number of words in password")
+  corpusPtr := flag.String("corpus", "corpus.txt", "newline sepearted file of words to use")
+  seedPtr := flag.String("seed", "", "seed for random generation, should allow repeat password generation")
+
+  flag.Parse()
+
+  var words []string
+  wordList := loadWordList(*corpusPtr)
+
+  var s rand.Source
+
+  if (*seedPtr != "") {
+  // if there's a seed flag, hash it, then use it
+    seed := hash(*seedPtr)
+    s = rand.NewSource(seed)
+  } else {
+  // if there's no seed flag just use something generic
+    s = rand.NewSource(time.Now().Unix())
+  }
+  
+  r := rand.New(s) // initialize local pseudorandom generator
+
+  for i := 0; i < *pwdLengthPtr; i++ {
+    words = append(words, randomWord(wordList, r))
+  }
+
+  password := strings.Join(words, *separatorPtr)
+  fmt.Println(password)
+
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,42 @@
-# Corpus how to
+# Battery-Apple 
+A silly (xkcd style password generator)[https://xkcd.com/936/] in go.
 
-1) Copy past words from this site: http://www.paulnoll.com/Books/Clear-English/English-3000-common-words.html
-2) remove double spaces and leading tabs:
+
+## Build
+
+Install (go via the offical instructions)[https://golang.org/doc/install]
+
+Build:
+
+    go build
+
+## Usage
+
+
+
+## Corpus how to recreate
+
+1) Copy & paste words from (this handy site)[http://www.paulnoll.com/Books/Clear-English/English-3000-common-words.html]. I did it manually, but I really should have used (this list)[https://github.com/first20hours/google-10000-english
+].
+2) Remove double spaces and leading tabs:
 
     sed "s/^[ \t]*//" -i corpus.txt
     sed "/^$/d" -i corpus.txt
 
-3) Remove the undesirable word (eg contractions, proper nouns)
+3) Remove the undesirable word (eg contractions, proper nouns). This may or may not have been a good idea.
 
     sed "/'/d" -i corpus.txt
     sed "/[A-Z]/d" -i corpus.txt
+
+4) Remove smaller words:
+
+    sed '/^\S\S$/d' -i corpus.txt
+    sed '/^\S\S\s$/d' -i corpus.txt
+
+
+## Sources
+
+[XKCD password strength](https://xkcd.com/936/)
+[XKCD password generator by preshing](http://preshing.com/20110811/xkcd-password-generator/)
+[Go Lang command line flags](https://gobyexample.com/command-line-flags)
+[A Tour of Go: flow control](https://tour.golang.org/flowcontrol/1)
