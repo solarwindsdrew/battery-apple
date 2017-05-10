50d3fe494ed689be141821ed144b88da6ba4ccd7
Drew Nelson <drewnelson112@gmail.com>
2017-05-09 23:01:11 -0400

    Refactor random. Fix default corpus location.

--- battery-apple.go
+++ battery-apple.go
@@ -38,16 +38,27 @@ func randomWord(wordList []string, r *rand.Rand) string {
 
 // Uses magic to create a hash of a string
 func hash(s string) int64 {
-        h := fnv.New64a()
-        h.Write([]byte(s))
-        return int64(h.Sum64())
+    h := fnv.New64a()
+    h.Write([]byte(s))
+    return int64(h.Sum64())
 }
+
+func randSource(s string) rand.Source {
+  if (s != "") {
+  // if there's a seed flag, hash it, then use it
+    seed := hash(s)
+    return rand.NewSource(seed)
+  } else {
+  // if there's no seed flag just use something generic
+    return rand.NewSource(time.Now().Unix())
+  }
+} 
 
 func main() {
 
   separatorPtr := flag.String("separator", "-", "character(s) to place between words")
   pwdLengthPtr := flag.Int("pwdLength", 4, "number of words in password")
-  corpusPtr := flag.String("corpus", "corpus.txt", "newline sepearted file of words to use")
+  corpusPtr := flag.String("corpus", "corpus/corpus.txt", "newline sepearted file of words to use")
   seedPtr := flag.String("seed", "", "seed for random generation, should allow repeat password generation")
 
   flag.Parse()
@@ -55,17 +66,8 @@ func main() {
   var words []string
   wordList := loadWordList(*corpusPtr)
 
-  var s rand.Source
-
-  if (*seedPtr != "") {
-  // if there's a seed flag, hash it, then use it
-    seed := hash(*seedPtr)
-    s = rand.NewSource(seed)
-  } else {
-  // if there's no seed flag just use something generic
-    s = rand.NewSource(time.Now().Unix())
-  }
-  
+  var s rand.Source = randSource(*seedPtr)
+   
   r := rand.New(s) // initialize local pseudorandom generator
 
   for i := 0; i < *pwdLengthPtr; i++ {
