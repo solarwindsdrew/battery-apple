9e0e7dee4193ef5cc39adf30fb8bd022e7abefcb
Drew Nelson <drew.nelson@solarwinds.com>
2018-03-10 21:01:29 -0500

    Updated loadWordList to use scanner (which is the _right_ way to do things). This also lets it deal with windows line ending correctly.

--- battery-apple.go
+++ battery-apple.go
@@ -1,77 +1,87 @@
 package main
 
 import (
-    "io/ioutil"
-    "fmt"
-    "flag"
-    "math/rand"
-    "time"
-    "strings"
-    "hash/fnv"
+	"bufio"
+	"flag"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
-
 
 // Retrieves a list of words from a newline seperated file
 // Returns an array of single words
 func loadWordList(corpusFile string) []string {
 
-  corpus, err := ioutil.ReadFile(corpusFile)
-  if err != nil {
-      panic(err)
-  }
-  words := strings.Split(string(corpus), "\n")
+	file, err := os.Open(corpusFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  return words
+	var words []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	return words
 }
 
 func randomWord(wordList []string, r *rand.Rand) string {
 
-  q := wordList[r.Intn(len(wordList))]
+	q := wordList[r.Intn(len(wordList))]
 
-  return q
+	return q
 }
 
 // Uses magic to create a hash of a string
 func hash(s string) int64 {
 
-  h := fnv.New64a()
-  h.Write([]byte(s))
-  return int64(h.Sum64())
+	h := fnv.New64a()
+	h.Write([]byte(s))
+	return int64(h.Sum64())
 }
 
-// Creates the randomization source 
+// Creates the randomization source
 func randSource(s string) rand.Source {
 
-  if (s != "") {
-  // if there's a seed flag, hash it, then use it
-    seed := hash(s)
-    return rand.NewSource(seed)
-  } else {
-  // if there's no seed flag just use something generic
-    return rand.NewSource(time.Now().Unix())
-  }
-} 
+	if s != "" {
+		// if there's a seed flag, hash it, then use it
+		seed := hash(s)
+		return rand.NewSource(seed)
+	} else {
+		// if there's no seed flag just use something generic
+		return rand.NewSource(time.Now().Unix())
+	}
+}
 
 func main() {
 
-  separatorPtr := flag.String("separator", "-", "character(s) to place between words")
-  pwdLengthPtr := flag.Int("pwdLength", 4, "number of words in password")
-  corpusPtr := flag.String("corpus", "corpus/corpus.txt", "newline sepearted file of words to use")
-  seedPtr := flag.String("seed", "", "seed for random generation, should allow repeat password generation")
+	separatorPtr := flag.String("separator", "-", "character(s) to place between words")
+	pwdLengthPtr := flag.Int("pwdLength", 4, "number of words in password")
+	corpusPtr := flag.String("corpus", "corpus/corpus.txt", "newline sepearted file of words to use")
+	seedPtr := flag.String("seed", "", "seed for random generation, should allow repeat password generation")
 
-  flag.Parse()
+	flag.Parse()
 
-  var words []string
-  wordList := loadWordList(*corpusPtr)
+	var words []string
+	wordList := loadWordList(*corpusPtr)
 
-  var s rand.Source = randSource(*seedPtr)
-  r := rand.New(s) // initialize local pseudorandom generator
+	var s rand.Source = randSource(*seedPtr)
+	r := rand.New(s) // initialize local pseudorandom generator
 
-  for i := 0; i < *pwdLengthPtr; i++ {
-    words = append(words, randomWord(wordList, r))
-  }
+	for i := 0; i < *pwdLengthPtr; i++ {
+		words = append(words, randomWord(wordList, r))
+	}
 
-  password := strings.Join(words, *separatorPtr)
-  fmt.Println(password)
+	password := strings.Join(words, *separatorPtr)
+	fmt.Println(password)
 
 }
